a1df2372fb84ce97b90b38f9656c78d9e2e8981a
AustinHadden <vitovalletori@gmail.com>
2019-12-17 16:13:24 -0600

    finished MVP part 2

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -6,9 +6,9 @@ const Users = require("../users/users-model.js");
 
 router.post("/register", (req, res) => {
   let user = req.body;
-  
+
   const hash = bcrypt.hashSync(user.password, 14);
- 
+
   user.password = hash;
 
   Users.add(user)
@@ -23,11 +23,11 @@ router.post("/register", (req, res) => {
 router.post("/login", (req, res) => {
   let { username, password } = req.body;
 
-  
   Users.findBy({ username })
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.user = user;
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: "You shall not pass!" });
@@ -36,6 +36,20 @@ router.post("/login", (req, res) => {
     .catch(error => {
       res.status(500).json(error);
     });
+});
+
+router.get("/logout", (req, res) => {
+  if (req.session) {
+    req.session.destroy(error => {
+      if (error) {
+        res.status(500).json({ message: "error destroying session" });
+      } else {
+        res.status(200).json({ message: "logged out" });
+      }
+    });
+  } else {
+    res.status(200).end();
+  }
 });
 
 module.exports = router;
